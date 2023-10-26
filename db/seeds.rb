c4a15213626721d42b6317054a301afd13d2f8ce
adinareiter <144619417+adinareiter@users.noreply.github.com>
2023-10-26 17:54:34 -0400

    the five RESTful actions practice

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+song = Song.create({
+  title: "ABC's",
+  album: "nonexistant",
+  artist: "Charles Bradlee",
+  year: 1835,
+})
+song.save
+
+song = Song.create({
+  title: "The Goldfish",
+  album: "Victor Vito",
+  artist: "Laurie Berkner",
+  year: 1999,
+})
+song.save
